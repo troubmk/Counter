117618a7dc6dc58aefb534974472c87b25ebb551
Pham Thu <troubmk@gmail.com>
2026-03-26 14:14:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [count, setCount] = useState(0);
+    let color;
+      if (count > 0) {
+    color = "green";
+  }
+  if (count < 0) {
+    color = "red";
+  }
+  if (count === 0) {
+    color = "black";
+  }
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    const reset = () => setCount(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="container">
+      <h1>Counter</h1>
+      <span style={{color}} id="value">{count}</span>
+      <div className="btn-group">
+        <button onClick={decrement} className="btn btn-decrease">DECREASE</button>
+        <button onClick={reset} className="btn btn-reset">RESET</button>
+        <button onClick={increment} className="btn btn-increase">INCREASE</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
